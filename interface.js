9a4bbe01d1fd123b72db5ad287ffef7abd611598
mrao2 <mohith.rao96@gmail.com>
2018-11-04 00:33:54 -0700

    Updated db info

--- interface.js
+++ interface.js
@@ -2,20 +2,20 @@ var pg = require("pg");
 
 var db = new pg.Pool({
 
-    user: 'scira',
+    user: 'ciurpxndehzlss',
 
-    host: '0.tcp.ngrok.io',
+    host: 'ec2-50-16-196-57.compute-1.amazonaws.com/',
 
-    database: 'postgres',
+    database: 'd3f2eb63bcb14q',
 
-    password: 'password',
+    password: '778341d687d94a8b5e1f0c38d566dfd1155cda512f298bf905f6250a64d40d90',
 
-    port: 19934,
+    port: 5432,
 
   });
 
 
-
+//Out
 db.query(`SELECT json_build_object(
   'type',       'Feature',
   'id',         r.ogc_fid,
@@ -39,7 +39,51 @@ WHERE fp.zone_subty in ('FLOODWAY', '0.2 PCT ANNUAL CHANCE FLOOD HAZARD', 'AREA 
     }
 
   })
-
+// Flood
+  db.query(`SELECT json_build_object(
+    'type',       'Feature',
+    'id',         fp.ogc_fid,
+    'geometry',   ST_AsGeoJSON(fp.wkb_geometry)::json,
+    'properties', json_build_object(
+        'zone_subty', fp.zone_subty
+     )
+ )
+FROM floodplain fp 
+WHERE fp.zone_subty in ('FLOODWAY', '0.2 PCT ANNUAL CHANCE FLOOD HAZARD', 'AREA WITH REDUCED FLOOD RISK DUE TO LEVEE')`, (err, res) => {
+  
+      if (err) {
+  
+        console.log(err.stack)
+  
+      } else {
+  
+        console.log(res.rows[0])
+  
+      }
+  
+    })
+//Roads
+    db.query(`SELECT json_build_object(
+      'type',       'Feature',
+      'id',         r.ogc_fid,
+      'geometry',   ST_AsGeoJSON(r.wkb_geometry)::json,
+      'properties', json_build_object(
+          'fullname', r.streetname
+       )
+   )
+  FROM roads r `, (err, res) => {
+    
+        if (err) {
+    
+          console.log(err.stack)
+    
+        } else {
+    
+          console.log(res.rows[0])
+    
+        }
+    
+      })
 
 
   db.end();
